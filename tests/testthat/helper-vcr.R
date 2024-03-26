ef6f1da168386a77f9bcc0386085d5f253c9f775
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2024-03-26 15:07:54 +0100

    chore: minor improvements

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -2,6 +2,6 @@ library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
   dir = vcr::vcr_test_path("fixtures"),
   filter_sensitive_data =
-    list("<<rorcid_bearer_token>>" = Sys.getenv('ORCID_TOKEN'))
+    list("<<rorcid_bearer_token>>" = Sys.getenv("ORCID_TOKEN"))
 ))
 vcr::check_cassette_names()
